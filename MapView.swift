5a35c97ffd80ca06bf88e83cd80f8a4242e355e9
cosmicfabri <fabrizzio86@outlook.es>
2025-08-27 11:48:02 -0600

    feat: Added map view; need to fix some issues.

---
+++ MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Paisajes
+//
+//  Created by Alumno 04 on 27/08/25.
+//
+
+import SwiftUI
+import MapKit
+
+
+
+struct MapView: View {
+    var body: some View {
+        Map(coordinateRegion: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 46.8182, longitude: 8.2275), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
