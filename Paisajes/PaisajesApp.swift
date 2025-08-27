0c5381c7108f4075b3875e1a95ffcf94f70c9dc4
E3 <alumno04@iMac-de-Facultad.local>
2025-08-27 10:56:28 -0600

    feat: Inicio de los tiempos.

---
+++ Paisajes/PaisajesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaisajesApp.swift
+//  Paisajes
+//
+//  Created by Alumno 04 on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PaisajesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
