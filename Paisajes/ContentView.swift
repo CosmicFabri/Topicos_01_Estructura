6f9290afb917596bf11237ce67dfbfde3d9a223c
cosmicfabri <fabrizzio86@outlook.es>
2025-08-27 11:15:21 -0600

    feat: Added VStack layout; added some Switzerland text.

--- Paisajes/ContentView.swift
+++ Paisajes/ContentView.swift
@@ -3,14 +3,23 @@
 //  Paisajes
 //
 //  Created by Alumno 04 on 27/08/25.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-        .padding()
+        VStack(alignment: .leading) {
+            Text("Suiza")
+                .font(.title)
+                .padding()
+                .foregroundColor(.green)
+            HStack {
+                Text("46.8182° N, 8.2275° E,")
+                    .font(.subheadline)
+                Text("Bern")
+            }
+            
+        }
     }
 }
 
