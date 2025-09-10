11ab6f59e9473ab624615990b82abcd635bd4987
cosmicfabri <fabrizzio86@outlook.es>
2025-09-10 10:39:03 -0600

    feat: Added image assets and Paisaje class.

---
+++ Paisajes/Paisaje.swift
@@ -0,0 +1,38 @@
+//
+//  Paisaje.swift
+//  Paisajes
+//
+//  Created by Alumno 04 on 10/09/25.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Paisaje: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var location: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    
+    var image: Image {
+        Image(imageName)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D (
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+}
